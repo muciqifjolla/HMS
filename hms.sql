e8ea4862886dc3ef48ac969615d473ef0b9995f7
“Alton <asllanialton@gmail.com>
2024-03-28 16:33:43 +0100

    tabelat nurse , room

---
+++ hms.sql
@@ -0,0 +1,423 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 28, 2024 at 04:28 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `Dept_ID` int(11) NOT NULL,
+  `Dept_head` varchar(100) DEFAULT NULL,
+  `Dept_name` varchar(100) NOT NULL,
+  `Emp_Count` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `Doctor_ID` int(11) NOT NULL,
+  `Qualifications` varchar(255) NOT NULL,
+  `Emp_ID` int(11) NOT NULL,
+  `Specialization` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emergency_contact`
+--
+
+CREATE TABLE `emergency_contact` (
+  `Contact_ID` int(11) NOT NULL,
+  `Contact_Name` varchar(100) NOT NULL,
+  `Phone` varchar(20) NOT NULL,
+  `Relation` varchar(50) NOT NULL,
+  `Patient_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `insurance`
+--
+
+CREATE TABLE `insurance` (
+  `Policy_Number` varchar(20) NOT NULL,
+  `Patient_ID` int(11) NOT NULL,
+  `Ins_Code` varchar(10) NOT NULL,
+  `End_Date` date DEFAULT NULL,
+  `Provider` varchar(100) NOT NULL,
+  `Plan` varchar(100) NOT NULL,
+  `Co_Pay` decimal(10,2) DEFAULT NULL,
+  `Coverage` varchar(255) DEFAULT NULL,
+  `Maternity` tinyint(1) DEFAULT NULL,
+  `Dental` tinyint(1) DEFAULT NULL,
+  `Optical` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lab_screening`
+--
+
+CREATE TABLE `lab_screening` (
+  `Lab_ID` int(11) NOT NULL,
+  `Patient_ID` int(11) NOT NULL,
+  `Technician_ID` int(11) DEFAULT NULL,
+  `Doctor_ID` int(11) DEFAULT NULL,
+  `Test_Cost` decimal(10,2) DEFAULT NULL,
+  `Date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medical_history`
+--
+
+CREATE TABLE `medical_history` (
+  `Record_ID` int(11) NOT NULL,
+  `Patient_ID` int(11) NOT NULL,
+  `Allergies` varchar(255) DEFAULT NULL,
+  `Pre_Conditions` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicine`
+--
+
+CREATE TABLE `medicine` (
+  `Medicine_ID` int(11) NOT NULL,
+  `M_name` varchar(100) NOT NULL,
+  `M_Quantity` int(11) NOT NULL,
+  `M_Cost` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nurse`
+--
+
+CREATE TABLE `nurse` (
+  `Nurse_ID` int(11) NOT NULL,
+  `Patient_ID` int(11) NOT NULL,
+  `Emp_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `Patient_ID` int(11) NOT NULL,
+  `Patient_Fname` varchar(50) NOT NULL,
+  `Patient_Lname` varchar(50) NOT NULL,
+  `Blood_type` varchar(10) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `Gender` varchar(10) DEFAULT NULL,
+  `Conditionn` varchar(255) DEFAULT NULL,
+  `Admission_Date` date NOT NULL,
+  `Discharge_Date` date DEFAULT NULL,
+  `Phone` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payroll`
+--
+
+CREATE TABLE `payroll` (
+  `Account_no` int(11) NOT NULL,
+  `Salary` decimal(10,2) NOT NULL,
+  `Bonus` decimal(10,2) DEFAULT NULL,
+  `Emp_ID` int(11) NOT NULL,
+  `IBAN` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `Room_ID` int(11) NOT NULL,
+  `Room_type` varchar(20) NOT NULL,
+  `Patient_ID` int(11) DEFAULT NULL,
+  `Room_cost` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `Emp_ID` int(11) NOT NULL,
+  `Emp_Fname` varchar(50) NOT NULL,
+  `Emp_Lname` varchar(50) NOT NULL,
+  `Joining_Date` date NOT NULL,
+  `Emp_type` varchar(50) NOT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `Dept_ID` int(11) DEFAULT NULL,
+  `SSN` varchar(20) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  `Date_Separation` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`Dept_ID`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`Doctor_ID`),
+  ADD KEY `Emp_ID` (`Emp_ID`);
+
+--
+-- Indexes for table `emergency_contact`
+--
+ALTER TABLE `emergency_contact`
+  ADD PRIMARY KEY (`Contact_ID`),
+  ADD KEY `Patient_ID` (`Patient_ID`);
+
+--
+-- Indexes for table `insurance`
+--
+ALTER TABLE `insurance`
+  ADD PRIMARY KEY (`Policy_Number`),
+  ADD KEY `Patient_ID` (`Patient_ID`);
+
+--
+-- Indexes for table `lab_screening`
+--
+ALTER TABLE `lab_screening`
+  ADD PRIMARY KEY (`Lab_ID`),
+  ADD KEY `Patient_ID` (`Patient_ID`),
+  ADD KEY `Doctor_ID` (`Doctor_ID`);
+
+--
+-- Indexes for table `medical_history`
+--
+ALTER TABLE `medical_history`
+  ADD PRIMARY KEY (`Record_ID`),
+  ADD KEY `Patient_ID` (`Patient_ID`);
+
+--
+-- Indexes for table `medicine`
+--
+ALTER TABLE `medicine`
+  ADD PRIMARY KEY (`Medicine_ID`);
+
+--
+-- Indexes for table `nurse`
+--
+ALTER TABLE `nurse`
+  ADD PRIMARY KEY (`Nurse_ID`),
+  ADD KEY `Patient_ID` (`Patient_ID`),
+  ADD KEY `Emp_ID` (`Emp_ID`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`Patient_ID`);
+
+--
+-- Indexes for table `payroll`
+--
+ALTER TABLE `payroll`
+  ADD PRIMARY KEY (`Account_no`),
+  ADD KEY `Emp_ID` (`Emp_ID`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`Room_ID`),
+  ADD KEY `Patient_ID` (`Patient_ID`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`Emp_ID`),
+  ADD KEY `Dept_ID` (`Dept_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `Dept_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `Doctor_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `emergency_contact`
+--
+ALTER TABLE `emergency_contact`
+  MODIFY `Contact_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lab_screening`
+--
+ALTER TABLE `lab_screening`
+  MODIFY `Lab_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `medical_history`
+--
+ALTER TABLE `medical_history`
+  MODIFY `Record_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `medicine`
+--
+ALTER TABLE `medicine`
+  MODIFY `Medicine_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nurse`
+--
+ALTER TABLE `nurse`
+  MODIFY `Nurse_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `Patient_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payroll`
+--
+ALTER TABLE `payroll`
+  MODIFY `Account_no` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `Room_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `Emp_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD CONSTRAINT `doctor_ibfk_1` FOREIGN KEY (`Emp_ID`) REFERENCES `staff` (`Emp_ID`);
+
+--
+-- Constraints for table `emergency_contact`
+--
+ALTER TABLE `emergency_contact`
+  ADD CONSTRAINT `emergency_contact_ibfk_1` FOREIGN KEY (`Patient_ID`) REFERENCES `patient` (`Patient_ID`);
+
+--
+-- Constraints for table `insurance`
+--
+ALTER TABLE `insurance`
+  ADD CONSTRAINT `insurance_ibfk_1` FOREIGN KEY (`Patient_ID`) REFERENCES `patient` (`Patient_ID`);
+
+--
+-- Constraints for table `lab_screening`
+--
+ALTER TABLE `lab_screening`
+  ADD CONSTRAINT `lab_screening_ibfk_1` FOREIGN KEY (`Patient_ID`) REFERENCES `patient` (`Patient_ID`),
+  ADD CONSTRAINT `lab_screening_ibfk_2` FOREIGN KEY (`Doctor_ID`) REFERENCES `doctor` (`Doctor_ID`);
+
+--
+-- Constraints for table `medical_history`
+--
+ALTER TABLE `medical_history`
+  ADD CONSTRAINT `medical_history_ibfk_1` FOREIGN KEY (`Patient_ID`) REFERENCES `patient` (`Patient_ID`);
+
+--
+-- Constraints for table `nurse`
+--
+ALTER TABLE `nurse`
+  ADD CONSTRAINT `nurse_ibfk_1` FOREIGN KEY (`Patient_ID`) REFERENCES `patient` (`Patient_ID`),
+  ADD CONSTRAINT `nurse_ibfk_2` FOREIGN KEY (`Emp_ID`) REFERENCES `staff` (`Emp_ID`);
+
+--
+-- Constraints for table `payroll`
+--
+ALTER TABLE `payroll`
+  ADD CONSTRAINT `payroll_ibfk_1` FOREIGN KEY (`Emp_ID`) REFERENCES `staff` (`Emp_ID`);
+
+--
+-- Constraints for table `room`
+--
+ALTER TABLE `room`
+  ADD CONSTRAINT `room_ibfk_1` FOREIGN KEY (`Patient_ID`) REFERENCES `patient` (`Patient_ID`);
+
+--
+-- Constraints for table `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`Dept_ID`) REFERENCES `department` (`Dept_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
