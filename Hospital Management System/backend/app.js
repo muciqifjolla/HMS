15d226cadcd2416ad48c12953fa1a50fce2cafaa
bledarxhemaili <103071946+bledarxhemaili@users.noreply.github.com>
2024-04-26 11:42:36 +0200

    Some changes

--- Hospital Management System/backend/app.js
+++ Hospital Management System/backend/app.js
@@ -10,17 +10,9 @@ const PatientRoutes = require('./routes/PatientRoutes');
 const DepartmentRoutes = require('./routes/DepartmentRoutes');
 const InsuranceRoutes = require('./routes/InsuranceRoutes');
 const StaffRoutes = require('./routes/StaffRoutes');
-<<<<<<< Updated upstream
 const MedicalHistoryRoutes = require('./routes/MedicalHistoryRoutes');
-=======
-<<<<<<< HEAD
 const RoomRoutes = require('./routes/RoomRoutes');
 
-=======
-const MedicalHistoryRoutes = require('./routes/MedicalHistoryRoutes');
->>>>>>> 9e117cc9e52bfb73d2d7766bc951bd508288b675
->>>>>>> Stashed changes
-// Other routes if needed...
 
 const app = express();
 app.use(express.json());
@@ -47,16 +39,7 @@ app.use("/api", PatientRoutes);
 app.use("/api", DepartmentRoutes);
 app.use("/api", InsuranceRoutes);
 app.use("/api", StaffRoutes);
-<<<<<<< Updated upstream
 app.use("/api", MedicalHistoryRoutes);
-=======
-<<<<<<< HEAD
 app.use("/api", RoomRoutes);
-=======
-app.use("/api", MedicalHistoryRoutes);
->>>>>>> 9e117cc9e52bfb73d2d7766bc951bd508288b675
->>>>>>> Stashed changes
-
-// Other routes if needed...
 
 module.exports = app;
--- Hospital Management System/src/routes.jsx
+++ Hospital Management System/src/routes.jsx
@@ -1,14 +1,8 @@
 
 
-<<<<<<< Updated upstream
-import { Home, Profile, Medicines, Appointments,MedicalHistory,Staffs, Patients, Departments, Emergency_Contacts, Insurances} from "@/pages/dashboard";
-=======
-<<<<<<< HEAD
-import { Home, Profile, Medicines, Appointments,Staffs, Patients, Departments, Emergency_Contacts, Insurances, Rooms} from "@/pages/dashboard";
-=======
-import { Home, Profile, Medicines, Appointments,MedicalHistory,Staffs, Patients, Departments, Emergency_Contacts, Insurances} from "@/pages/dashboard";
->>>>>>> 9e117cc9e52bfb73d2d7766bc951bd508288b675
->>>>>>> Stashed changes
+
+import { Home, Profile, Medicines, Appointments,MedicalHistory,Staffs, Patients, Departments, Emergency_Contacts, Insurances, Rooms} from "@/pages/dashboard";
+
 // import { SignIn, SignUp } from "@/pages/auth";
 
 
@@ -66,8 +60,8 @@ export const routes = [
       {
         icon: (
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-        </svg>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+          </svg>
 
         ),
         name: "medical history",
@@ -77,8 +71,8 @@ export const routes = [
       {
         icon: (
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-        </svg>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+          </svg>
 
         ),
 
